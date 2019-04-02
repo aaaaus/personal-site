ccd0fc6d1d25cc3fa512b5d1287da59405712f77
aaaaus <ausluft@gmail.com>
2019-04-01 21:48:37 -0400

    added more thumbnails

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -34,7 +34,7 @@ class ContentContainer extends React.Component {
   //function renders all photos of matching location type
   photoRender() {
 
-    const filteredPhotos = photos.filter(photo => photo.location === this.props.location)
+    const filteredPhotos = photos.filter(photo => photo.location === this.props.location && photo.feature === false)
 
     return filteredPhotos.map(photo => <Photo className="gallery-image" photo={photo} key={photo.photoid} />)
 
